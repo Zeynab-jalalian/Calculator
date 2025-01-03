4e1efa4de1d64970ac2f03c34a4d314c9a24f86f
zeynab jalalian <zjalalianweb@gmail.com>
2025-01-03 13:02:27 +0330

    some html css js code...

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 (function(){
-    alert('ok')
+   let display=document.querySelector('display');
+   let buttons=document.querySelectorAll('btn');
+   let clear=document.querySelector('#clear');
+   let equal=document.querySelector('#equal');
 })();
