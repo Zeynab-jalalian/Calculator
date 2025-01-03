31ebcd82ba452d66fc8a3eea9d9f0af4cee4868a
zeynab jalalian <zjalalianweb@gmail.com>
2025-01-03 15:01:50 +0330

    finish

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 (function(){
-   let display=document.querySelector('#display');
-   let buttons=document.querySelectorAll('.btn');
-   let clear=document.querySelector('#clear');
-   let equal=document.querySelector('#equal');
+ let display=document.querySelector('#display');
+ let buttons=document.querySelectorAll('.btn');
+ let clear=document.querySelector('#clear');
+ let equal=document.querySelector('#equal');
+
+buttons.forEach(function(button){
+    button.addEventListener('click',function(e){
+        if(display.value==="empty"){
+            display.value="";
+            let value=e.target.defaultValue;
+            display.value+=value;
+        }
+        else{
+            let value=e.target.defaultValue;
+            display.value+=value;
+        }
+       
+    })
+    equal.addEventListener('click',function(e){
+        if(display.value===""){
+            display.value="empty";
+        }else{
+           let answer=eval(display.value);
+           display.value=answer;
+        }
+    })
+    clear.addEventListener('click',function(e){
+        display.value="";
+    })
+})
 })();
