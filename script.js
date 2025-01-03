8618f6c965f6a8f0c76763dc90d174828d538c7a
zeynab jalalian <zjalalianweb@gmail.com>
2025-01-03 13:12:48 +0330

    fix bug

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 (function(){
-   let display=document.querySelector('display');
-   let buttons=document.querySelectorAll('btn');
+   let display=document.querySelector('#display');
+   let buttons=document.querySelectorAll('.btn');
    let clear=document.querySelector('#clear');
    let equal=document.querySelector('#equal');
 })();
